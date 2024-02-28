84d2a9a9989a3e77a5c6e2a0e10d8516b798eb83
sachinaggerwal <sachinandcharlie@gmail.com>
2024-02-28 15:50:07 -0500

    forgot to add commas

--- IRM_3004/culinaryCorner/urls.py
+++ IRM_3004/culinaryCorner/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="homepage"),
-    path('about/',views.about,name="cc_about")
-    path('faq/',views.about,name="cc_faq")
-    path('contact/',views.about,name="cc_contact")
+    path('about/',views.about,name="cc_about"),
+    path('faq/',views.about,name="cc_faq"),
+    path('contact/',views.about,name="cc_contact"),
     path('orders/',views.about,name="cc_about")
 ]
