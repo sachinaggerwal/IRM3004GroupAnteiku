cf746a270f74ebdc628a2c811c85fdf8a1523cca
sachinaggerwal <sachinandcharlie@gmail.com>
2024-02-28 16:03:21 -0500

    testing html pages

--- IRM_3004/culinaryCorner/views.py
+++ IRM_3004/culinaryCorner/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'culinaryCorner/homepage.html')
+
+def about(request):
+    return render(request, 'culinaryCorner/about.html')
