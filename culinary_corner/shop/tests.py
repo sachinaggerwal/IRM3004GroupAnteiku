d2d84fb018d2ac65af61d23b363586e3dc796a4b
sachinaggerwal <sachinandcharlie@gmail.com>
2024-03-22 13:03:25 -0400

    adding test cases

--- culinary_corner/shop/tests.py
+++ culinary_corner/shop/tests.py
@@ -2,5 +2,26 @@
 This is the tests module
 '''
 from django.test import TestCase
-
+from .apps import ShopConfig
+from .models import Product,Contact,Orders,OrderUpdate
+from datetime import date
 # Create your tests here.
+class ShopConfigTest(TestCase):
+    def test_app_name(self):
+        self.assertEqual(ShopConfig.name,"shop")
+
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(
+            product_name = "test product",
+            category = "test",
+            desc = "test description",
+            price = 10,
+            pub_date = date.today()
+        )
+
+    def test_product_name_label(self):
+        product = Product.objects.get(id = 1)
+        field_label = product._meta.get_field('product_name').verbose_name
+        self.assertEquals(field_label,"product name")
